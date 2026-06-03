6f2235e51685b054cdd5dc927b899dfc864ea0e9
Yana <assistant.yanaa@gmail.com>
2026-06-03 16:18:52 +0400

    change

--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -24,8 +24,8 @@ public class CreateUserTest {
     public void createUser() {
         User user = new User();
         user.setUsername(RandomUtils.getRandomAlphabeticString());
-        user.setFirstName(RandomUtils.getRandomAlphabeticString());
-        user.setLastName(RandomUtils.getRandomAlphabeticString());
+        user.setFirstName("Yana");
+        user.setLastName("Tkemaladze");
         user.setEmail(RandomUtils.getRandomAlphabeticString());
         user.setPassword(RandomUtils.getRandomAlphabeticString());
         user.setPhone(RandomUtils.getRandomAlphabeticString());
